d7526971c2d1ba58e0775b48a792039068c85192
Cinzya <cynti-ebert@web.de>
2024-10-21 23:31:31 +0200

    feat: add eventlistener to filter on button click

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.onload = async () => {
   let data = [];
+
+  // Fetch data
   try {
     const response = await fetch("./products.json");
     data = await response.json();
@@ -7,8 +9,22 @@ window.onload = async () => {
   } catch (error) {
     console.error("Error while fetching data", error);
   }
+
+  // Filter on button click
+  const buttons = document.querySelectorAll("button");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const category = button.getAttribute("id");
+      const filteredData = filterByCategory(data, category);
+      console.log("Filtered data", filteredData);
+    });
+  });
 };
 
 function filterByCategory(data, category) {
-  return data.filter((product) => product.category === category);
+  if (category === "All") {
+    return data;
+  }
+  return data.filter((item) => item.category === category);
 }
