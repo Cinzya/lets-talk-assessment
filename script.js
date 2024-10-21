635bc2f34f6b18aaf27792de75db93a1c926cf44
Cinzya <cynti-ebert@web.de>
2024-10-21 23:08:09 +0200

    feat: add filterByCategory function

--- script.js
+++ script.js
@@ -8,3 +8,7 @@ window.onload = async () => {
     console.error("Error while fetching data", error);
   }
 };
+
+function filterByCategory(data, category) {
+  return data.filter((product) => product.category === category);
+}
